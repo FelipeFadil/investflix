450a58dddd47c7dedf42a4954eb1248f02885bc3
FelipeFadil <felipefadil@gmail.com>
2020-07-28 20:34:01 -0300

    Add Carousel e Slider

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,6 +7,22 @@ function CadastroCategoria() {
       <PageDefault>
         <h1>Cadastro de categoria</h1>
 
+        <form>
+
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+            />
+          </label>
+
+          <button>
+            Cadastrar
+          </button>
+        </form>
+
+
+
         <Link to="/">
             Ir para home
         </Link>
